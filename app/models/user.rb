e258acbdd17ff1f06b6d1a78729de6c1ce185414
Nhat Huy <huydqn@ominext.com>
2024-04-09 21:21:54 +0700

    add user statuses for online, offline and away

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :messages
-
   has_one_attached :avatar
+
+  enum status: { offline: 0, online: 1, away: 2 }
 
   after_commit :add_default_avatar, on: [:create, :update]
 
   # broadcast a new user is created in users channel
   # won't have to refresh the page to see the new user appear
   after_create_commit { broadcast_append_to :users, target: 'js-users' }
+  after_update_commit { broadcast_replace_to :user_status, partial: 'users/status', user: self }
 
   scope :all_except, -> (user) { where.not(id: user) }
 
@@ -21,6 +23,17 @@ class User < ApplicationRecord
 
   def chat_avatar
     avatar.variant(resize_to_limit: [50, 50]).processed
+  end
+
+  def status_to_css
+    case status_before_type_cast
+    when 0
+      'bg-dark'
+    when 1
+      'bg-success'
+    else
+      'bg-warning'
+    end
   end
 
   private
